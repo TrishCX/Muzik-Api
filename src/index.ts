fbaf6a5be8ae9ac672a2dc07d63c7f70be2a4bc2
Ritesh <119659853+TrishCX@users.noreply.github.com>
2024-01-04 23:04:48 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,6 @@
-import { quickVideo } from "./api";
+import Express from "./classes/Express";
+import app from "./app/app";
 
-(async () => {
-  const query: string = `Daft Punk Music Videos`;
+const server = new Express(app);
 
-  const response = await quickVideo(query);
-  console.log(response);
-})();
-
-// import Express from "./classes/Express";
-// import app from "./app/app";
-
-// const server = new Express(app);
-
-// server.register();
+server.register();
